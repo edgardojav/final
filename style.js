cef1e3bf5dba26cbd6df5cfeca8da8794a1078a4
edgardo-jag <67526741+edgardo-jag@users.noreply.github.com>
2023-01-31 21:41:32 -0300

    rompecabezas

--- style.js
+++ style.js
@@ -1,3 +1,4 @@
+
 let video=document.querySelector(".videouno")
 const iniciaPlay=()=>{
     document.querySelector(".duracion").innerText="Duracion video:"+" "+video.duration
@@ -5,4 +6,26 @@ const iniciaPlay=()=>{
 }
 const iniciaPause=()=>{
     video.pause()
+}
+//juguemos
+
+let arreglo=["","",""];
+//evita el enlace
+function allowDrop(ev){
+   ev.preventDefault();
+};
+//arrastre del elemento
+function drag(ev){
+   ev.dataTransfer.setData("text",ev.target.id);
+}
+
+function drop(ev){
+    if(arreglo[parseInt(ev.target.id)] ==""){
+        let data = ev.dataTransfer.getData("text");
+        arreglo[parseInt(ev.target.id)]=data;
+        ev.target.appendChild(document.getElementById(data))
+    }
+}
+function recarga(){
+    window.location.reload()
 }
